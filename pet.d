b917112df756624fe5b2099fa61ec517bc8c61ad
Francesco Paolo Carmone <enforcetitan@gmail.com>
2023-01-17 12:05:47 +0100

    Revert "rimossi file inutili"
    This reverts commit a572932454915d4beb2064cc56810dff8a4b4253.

---
+++ pet.d
@@ -0,0 +1,11 @@
+.\pet.o: tamagotchi\pet.c
+.\pet.o: C:\Keil_v5\ARM\ARMCLANG5\Bin\..\include\stdint.h
+.\pet.o: tamagotchi\pet.h
+.\pet.o: tamagotchi\../GLCD/GLCD.h
+.\pet.o: C:\Users\Enf\AppData\Local\Arm\Packs\Keil\LPC1700_DFP\2.7.1\Device\Include\LPC17xx.h
+.\pet.o: C:\Users\Enf\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\core_cm3.h
+.\pet.o: C:\Users\Enf\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_version.h
+.\pet.o: C:\Users\Enf\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_compiler.h
+.\pet.o: C:\Users\Enf\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_armcc.h
+.\pet.o: C:\Users\Enf\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\mpu_armv7.h
+.\pet.o: C:\Users\Enf\AppData\Local\Arm\Packs\Keil\LPC1700_DFP\2.7.1\Device\Include\system_LPC17xx.h
